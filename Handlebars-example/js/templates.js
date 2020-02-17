15a32b0af803bcfe8b5aa398c3d5a5e6da76c755
eidman <eidmanx@gmail.com>
2020-02-17 17:04:16 -0500

    -

--- Handlebars-example/js/templates.js
+++
@@ -1,8 +0,0 @@
-const helloTemplate = Handlebars.compile(`
-  <div>
-    <a href='{{navbar.link}}'>{{navbar.linkName}}</a>
-    <h3>This says my name: </h3>
-    <p>Hello, My name is {{firstName}} {{lastName}}</p>
-    </p>My age is: {{age}}</p>
-  </div>
-`);
