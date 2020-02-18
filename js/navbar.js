e8e40978946cf0b4470622575447a3fecf8c0fc1
eidman <eidmanx@gmail.com>
2020-02-18 09:18:05 -0500

    fixed nav + other small things

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   var hamburger = document.querySelector('.hamburger');
   var navMenu = document.querySelector('.nav-menu-container');
-
   hamburger.addEventListener('click', function() {
-    hamburger.classList.add('is-active');
-    navMenu.classList.add('show');
+    hamburger.classList.toggle('is-active');
+    navMenu.classList.toggle('show');
   });
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     let hamburger = document.querySelectorAll('.hamburger');
-//     let navMenu = document.querySelector('.nav-menu-container');
-
-//     hamburger.forEach(function (button) {
-//         button.addEventListener('click', function () {
-//             hamburger.classList.add('is-active');
-//             navMenu.classList.add('show');
-//         });
-//     });
-// });
