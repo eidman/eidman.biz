55111a104df5bd9c3da3fb3efd959b661f700957
eidman <eidmanx@gmail.com>
2020-02-19 18:48:18 -0500

    fixing responsivity bugs

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var hamburger = document.querySelector('.hamburger');
-  var navMenu = document.querySelector('.nav-menu-container');
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('.nav-menu-container');
+  const scrollY = document.body.style.top;
+
   hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active');
     navMenu.classList.toggle('show');
+
+    // document.body.style.position = '';
+    // document.body.style.top = '';
+    // window.scrollTo(0, parseInt(scrollY || '0') * -1);
   });
 });
