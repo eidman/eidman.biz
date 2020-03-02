1e80f2123930682df2cc370b40d4096aad3829b1
eidman <eidmanx@gmail.com>
2020-03-02 13:58:09 -0500

    fixed body on modal

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu-container');
-  const scrollY = document.body.style.top;
+  const body = document.body;
 
   hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active');
     navMenu.classList.toggle('show');
+    body.classList.toggle('fixed');
   });
 
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
