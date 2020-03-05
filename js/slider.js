33123d20ed19e8e18ae2be75e25ae993ebc12d6c
eidman <eidmanx@gmail.com>
2020-03-05 10:23:00 -0500

    arrow test

--- js/slider.js
+++ js/slider.js
@@ -24,7 +24,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var flkty = new Flickity(carousel, {
       // selectedAttraction: 0.3,
       // friction: 1
-      draggable: false
+      draggable: false,
+      arrowShape: {
+        x0: 10,
+        x1: 60,
+        y1: 50,
+        x2: 70,
+        y2: 40,
+        x3: 30
+      }
     });
   }
 
