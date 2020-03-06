01e761562d8ef0119da28145c784b308335a6b10
eidman <eidmanx@gmail.com>
2020-03-05 23:51:25 -0500

    flickity js

--- js/slider.js
+++ js/slider.js
@@ -23,41 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // var flkty = $gallery.data('flickity');
-  // // load all lazy load images
-  // if (!flkty.isActive) {
-  //   $gallery.find('[data-flickity-lazyload]').each(function (i, elem) {
-  //     var $elem = $(elem);
-  //     $elem.attr('src', $elem.attr('data-flickity-lazyload'));
-  //     $elem.addClass('flickity-lazyloaded');
-  //     elem.removeAttribute('data-flickity-lazyload');
-  //   });
-
-  let slide = document.querySelectorAll('.carousel-cell-image');
-  // load all lazy load images
-  if (matchMedia('screen and (max-width: 868px)').matches) {
-    slide.forEach(function(image) {
-      // image.addClass('flickity-lazyloaded');
-      image.removeAttribute('data-flickity-lazyload');
-    });
-  }
-
-  // if (matchMedia('screen and (max-width: 768px)').matches) {
-  //   var flkty = new Flickity(carousel, {
-  //     selectedAttraction: 0.3,
-  //     friction: 1
-  //     draggable: false,
-  //     arrowShape: {
-  //       x0: 10,
-  //       x1: 60,
-  //       y1: 50,
-  //       x2: 70,
-  //       y2: 40,
-  //       x3: 30
-  //     }
-  //   });
-  // }
-
   var previousSlide = document.querySelector('.previous-big-button');
   var nextSlide = document.querySelector('.next-big-button');
 
